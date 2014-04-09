e5ac119344566e6aeac4e8404e19488b932b11fe
Hector <hjavierog@gmail.com>
2014-04-09 21:18:00 +0200

    General structure.

--- js/index.js
+++ js/index.js
@@ -11,7 +11,7 @@ jQuery(document).ready(function () {
     jQuery('nav').find('button').click(function () {
         openNavigationPanel();
     });
-    
+
     var $webNavPanel = jQuery('#webNavPanel');
     $webNavPanel.find('button').click(function () {
         closeNavigationPanel();
@@ -20,6 +20,10 @@ jQuery(document).ready(function () {
     $webNavPanel.find('li').click(function () {
         closeNavigationPanel();
     });
+
+    // Scrolling events
+    jQuery(window).scroll(scrollController);
+
 });
 
 function openNavigationPanel() {
@@ -27,4 +31,30 @@ function openNavigationPanel() {
 }
 function closeNavigationPanel() {
     jQuery('#webNavPanel').slideUp();
+}
+
+function scrollController() {
+    var currentPosition = jQuery(window).scrollTop();
+    // navPositioning(currentPosition);
+}
+
+function navPositioning(currentPosition) {
+    var $nav = jQuery('nav');
+    var $mainHeader = jQuery('#mainHeader');
+    var mainHeaderMiddle = $mainHeader.offset().top + ($mainHeader.height() / 2);
+
+    if (currentPosition >= mainHeaderMiddle && $nav.css('position') == 'relative') {
+        console.log('to fixed');
+        $nav.css('position', 'fixed');
+        $nav.animate({
+            'background-color': '#8A2C13'
+        });
+    } else if (currentPosition < mainHeaderMiddle && $nav.css('position') == 'fixed') {
+        console.log('to relative');
+        $nav.css('position', 'relative');
+        $nav.animate({
+            'background-color': 'transparent'
+        });
+
+    }
 }
